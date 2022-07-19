c132b23cc65fa31065495d2dae445f2b962cb574
Hutan <31436976+huti26@users.noreply.github.com>
2022-07-19 22:37:00 +0200

    performance improvement

--- project/src/main/java/hutan/tasks/SumCasesBySex.java
+++ project/src/main/java/hutan/tasks/SumCasesBySex.java
@@ -15,26 +15,27 @@ public class SumCasesBySex {
     public static PDone calculate(PCollection<String> input) {
 
         return input
-                .apply("Extract fields: KV(12:neuerFall, 5:geschlecht + , + 6:anzahlFall)",
+                .apply("KV(12:neuerFall, KV(5:geschlecht, 6:anzahlFall))",
                         MapElements
-                                .into(TypeDescriptors.kvs(TypeDescriptors.integers(), TypeDescriptors.strings()))
+                                .into(TypeDescriptors.kvs(TypeDescriptors.integers(), TypeDescriptors.kvs(TypeDescriptors.strings(), TypeDescriptors.integers())))
                                 .via(line -> {
                                     var fields = line.split(",");
-                                    return KV.of(Integer.parseInt(fields[12]), fields[5] + "," + fields[6]);
+                                    return KV.of(Integer.parseInt(fields[12]), KV.of(fields[5], Integer.parseInt(fields[6])));
                                 }))
-                .apply("Remove non new cases", Filter.by(element -> element.getKey() >= 0))
-                .apply("Convert key value pairs to strings",
-                        MapElements.into(TypeDescriptors.strings()).via(element -> element.getKey() + "," + element.getValue()))
-                .apply("Remove neuerFall:  KV(1:geschlecht, 2:anzahlFall)",
+                .apply("Remove non new cases",
+                        Filter.by(element -> element.getKey() >= 0))
+                .apply("Remove neuerFall: KV(12:neuerFall, KV(5:geschlecht, 6:anzahlFall)) -> KV(5:geschlecht, 6:anzahlFall)",
                         MapElements
                                 .into(TypeDescriptors.kvs(TypeDescriptors.strings(), TypeDescriptors.integers()))
-                                .via(line -> {
-                                    var fields = line.split(",");
-                                    return KV.of(fields[1], Integer.parseInt(fields[2]));
+                                .via(element -> {
+                                    return KV.of(element.getValue().getKey(), element.getValue().getValue());
                                 }))
-                .apply("Sum the amount of cases", Sum.integersPerKey())
+                .apply("Sum the amount of cases",
+                        Sum.integersPerKey())
                 .apply("Convert key value pairs to strings",
-                        MapElements.into(TypeDescriptors.strings()).via(element -> element.getKey() + "," + element.getValue()))
+                        MapElements
+                                .into(TypeDescriptors.strings())
+                                .via(element -> element.getKey() + "," + element.getValue()))
                 .apply("Write to file",
                         TextIO.write().to("pipeline_results/cases_by_sex.csv").withoutSharding());
 
