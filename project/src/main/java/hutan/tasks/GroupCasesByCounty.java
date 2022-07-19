2feab964566cf30f66008a2e39f15cd897ac7734
Hutan <31436976+huti26@users.noreply.github.com>
2022-07-19 21:37:27 +0200

    improve performance groupcasesbycounty

--- project/src/main/java/hutan/tasks/GroupCasesByCounty.java
+++ project/src/main/java/hutan/tasks/GroupCasesByCounty.java
@@ -15,22 +15,26 @@ public class GroupCasesByCounty {
     public static PDone calculate(PCollection<String> input) {
 
         return input
-                .apply("Extract fields 2:bundesland & 6:anzahlFall as KV, 12:neuerFall",
-                        MapElements.into(TypeDescriptors.kvs(TypeDescriptors.integers(), TypeDescriptors.kvs(TypeDescriptors.strings(), TypeDescriptors.integers())))
+                .apply("Extract fields: KV(12:neuerFall, KV(2:bundesland, 6:anzahlFall)",
+                        MapElements
+                                .into(TypeDescriptors.kvs(TypeDescriptors.integers(), TypeDescriptors.kvs(TypeDescriptors.strings(), TypeDescriptors.integers())))
                                 .via(line -> {
                                     var fields = line.split(",");
-                                    var bundesland = fields[2];
-                                    var anzahlFall = Integer.parseInt(fields[6]);
-                                    var neuerfall = Integer.parseInt(fields[12]);
-                                    return KV.of(neuerfall, KV.of(bundesland, anzahlFall));
+                                    return KV.of(Integer.parseInt(fields[12]), KV.of(fields[2], Integer.parseInt(fields[6])));
                                 }))
                 .apply("Remove non new cases", Filter.by(element -> element.getKey() >= 0))
                 .apply("Unnest KV -> Remove neuerFall field",
-                        MapElements.into(TypeDescriptors.kvs(TypeDescriptors.strings(), TypeDescriptors.integers()))
-                                .via(element -> KV.of(element.getValue().getKey(), element.getValue().getValue())))
+                        MapElements
+                                .into(TypeDescriptors.kvs(TypeDescriptors.strings(), TypeDescriptors.integers()))
+                                .via(element -> KV.of(
+                                        element.getValue().getKey(),
+                                        element.getValue().getValue())
+                                ))
                 .apply("Sum the amount of cases", Sum.integersPerKey())
                 .apply("Convert key value pairs to strings",
-                        MapElements.into(TypeDescriptors.strings()).via(element -> element.getKey() + ";" + element.getValue()))
+                        MapElements
+                                .into(TypeDescriptors.strings())
+                                .via(element -> element.getKey() + ";" + element.getValue()))
                 .apply("Write to file",
                         TextIO.write().to("pipeline_results/cases_by_county.csv").withoutSharding());
 
