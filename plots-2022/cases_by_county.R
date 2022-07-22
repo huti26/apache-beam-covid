cad1edefb7adf78c88f2241a8e4036667960ec3d
Hutan <31436976+huti26@users.noreply.github.com>
2022-07-22 19:47:34 +0200

    update

--- plots-2022/cases_by_county.R
+++ plots-2022/cases_by_county.R
@@ -27,10 +27,41 @@ data$Infektionen.millionen = data$Infektionen / 1000000
 #   )
 
 
+# g <-
+#   ggplot(data, aes(
+#     x = reorder(Bundesland, Infektionen, decreasing = TRUE),
+#     y = Infektionen.millionen
+#   )) +
+#   geom_bar(
+#     stat = "identity",
+#     width = 1,
+#     color = "white",
+#     position = "dodge"
+#   ) +
+#   labs(
+#     title = "Absolute number of Infections per County",
+#     subtitle = "Germany - 17.07.2022",
+#     x = "",
+#     y = "Million Infections"
+#   ) +
+#   theme(axis.text.x = element_text(
+#     angle = 90,
+#     vjust = 0.5,
+#     hjust = 1),
+#     text = element_text(size = 14),
+#     plot.margin = margin(t = 1, r = 2, b = 1, l = 1, unit = "cm"
+#     ))
+# 
+# 
+# 
+# print(g)
+
+# ggsave("cases_by_county.png", width = 8.37, height = 6, dpi = 300)
+
 g <-
   ggplot(data, aes(
-    x = reorder(Bundesland, Infektionen, decreasing = TRUE),
-    y = Infektionen.millionen
+    y = reorder(Bundesland, Infektionen, decreasing = FALSE),
+    x = Infektionen.millionen
   )) +
   geom_bar(
     stat = "identity",
@@ -41,15 +72,12 @@ g <-
   labs(
     title = "Absolute number of Infections per County",
     subtitle = "Germany - 17.07.2022",
-    x = "",
-    y = "Million Infections"
+    x = "Million Infections",
+    y = ""
   ) +
-  theme(axis.text.x = element_text(
-    angle = 90,
-    vjust = 0.5,
-    hjust = 1),
+  theme(
     text = element_text(size = 14),
-    plot.margin = margin(t = 1, r = 2, b = 1, l = 1, unit = "cm"
+    plot.margin = margin(t = 0.5, r = 1, b = 0.5, l = 0, unit = "cm"
     ))
 
 
@@ -57,4 +85,3 @@ g <-
 print(g)
 
 ggsave("cases_by_county.png", width = 8.37, height = 6, dpi = 300)
-
--- plots-2022/cases_by_county_relative.R
+++ plots-2022/cases_by_county_relative.R
@@ -44,10 +44,41 @@ data$per.100k.capita = data$relative.cases * 1000000
 # 
 # print(g)
 
+# g <-
+#   ggplot(data, aes(
+#     x = reorder(county, cases, decreasing = TRUE),
+#     y = relative.cases
+#   )) +
+#   geom_bar(
+#     stat = "identity",
+#     width = 1,
+#     color = "white",
+#     position = "dodge"
+#   ) +
+#   labs(
+#     title = "Relative number of Infections per County",
+#     subtitle = "Germany - 17.07.2022",
+#     x = "",
+#     y = "Relative Infections"
+#   ) +
+#   theme(axis.text.x = element_text(
+#     angle = 90,
+#     vjust = 0.5,
+#     hjust = 1),
+#     text = element_text(size = 14),
+#     plot.margin = margin(t = 1, r = 2, b = 1, l = 1, unit = "cm"
+#   ))
+# 
+# 
+# 
+# print(g)
+# 
+# ggsave("cases_by_county_relative.png", width = 8.37, height = 6, dpi = 300)
+
 g <-
   ggplot(data, aes(
-    x = reorder(county, cases, decreasing = TRUE),
-    y = relative.cases
+    y = reorder(county, cases, decreasing = FALSE),
+    x = relative.cases
   )) +
   geom_bar(
     stat = "identity",
@@ -58,19 +89,17 @@ g <-
   labs(
     title = "Relative number of Infections per County",
     subtitle = "Germany - 17.07.2022",
-    x = "",
-    y = "Relative Infections"
+    x = "Relative Infections",
+    y = ""
   ) +
-  theme(axis.text.x = element_text(
-    angle = 90,
-    vjust = 0.5,
-    hjust = 1),
+  theme(
     text = element_text(size = 14),
-    plot.margin = margin(t = 1, r = 2, b = 1, l = 1, unit = "cm"
-  ))
+    plot.margin = margin(t = 0.5, r = 1, b = 0.5, l = 0, unit = "cm"
+    ))
 
 
 
 print(g)
 
 ggsave("cases_by_county_relative.png", width = 8.37, height = 6, dpi = 300)
+
